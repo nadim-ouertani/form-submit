c672134e5af02f78e520432a8f2291b81ef311d3
Nadim Ouertani <nadimouertani@hotmail.com>
2022-05-29 00:18:12 +0400

    Create store subscriber controller

---
+++ app/Http/Controllers/formController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class formController extends Controller
+{
+    public function storeNewSubscriber(Request $request) {
+        dd($request);
+    }
+}
