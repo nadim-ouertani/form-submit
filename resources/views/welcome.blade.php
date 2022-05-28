9bed3405fb2634a5ef404979ddbb2fce38a9dfd8
Nadim Ouertani <nadimouertani@hotmail.com>
2022-05-29 00:23:10 +0400

    Update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,30 +6,30 @@
         <title>Laravel</title>
     </head>
     <body class="antialiased">
-        <form class="pure-form pure-form-aligned">
+        <form>
             <fieldset>
-                <div class="pure-control-group">
+                <div>
                     <label for="aligned-name">Username</label>
                     <input type="text" id="aligned-name" placeholder="Username" />
-                    <span class="pure-form-message-inline">This is a required field.</span>
+                    <span>This is a required field.</span>
                 </div>
-                <div class="pure-control-group">
+                <div>
                     <label for="aligned-password">Password</label>
                     <input type="password" id="aligned-password" placeholder="Password" />
                 </div>
-                <div class="pure-control-group">
+                <div>
                     <label for="aligned-email">Email Address</label>
                     <input type="email" id="aligned-email" placeholder="Email Address" />
                 </div>
-                <div class="pure-control-group">
+                <div>
                     <label for="aligned-foo">Supercalifragilistic Label</label>
                     <input type="text" id="aligned-foo" placeholder="Enter something here..." />
                 </div>
-                <div class="pure-controls">
+                <div>
                     <label for="aligned-cb" class="pure-checkbox">
                         <input type="checkbox" id="aligned-cb" /> I&#x27;ve read the terms and conditions
                     </label>
-                    <button type="submit" class="pure-button pure-button-primary">Submit</button>
+                    <button type="submit">Submit</button>
                 </div>
             </fieldset>
         </form>
